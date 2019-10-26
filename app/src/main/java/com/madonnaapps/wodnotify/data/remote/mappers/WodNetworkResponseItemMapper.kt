82d389874f630299910c3aaceb9d77739a133994
Tyler Madonna <developer@madonnaapps.com>
2019-10-26 18:28:14 -0400

    Created mapper for mapping remote wod models to local wod entities

---
+++ app/src/main/java/com/madonnaapps/wodnotify/data/remote/mappers/WodNetworkResponseItemMapper.kt
@@ -0,0 +1,41 @@
+package com.madonnaapps.wodnotify.data.remote.mappers
+
+import com.madonnaapps.wodnotify.common.extensions.parseOrNull
+import com.madonnaapps.wodnotify.data.local.entities.WodEntity
+import com.madonnaapps.wodnotify.data.remote.models.WodNetworkResponseItem
+import java.text.SimpleDateFormat
+import java.util.*
+
+class WodNetworkResponseItemMapper(
+    private val dateFormats: Array<SimpleDateFormat>,
+    private val baseUrl: String
+) {
+
+    fun mapToWodEntity(responseItem: WodNetworkResponseItem): WodEntity? {
+        return WodEntity(
+            responseItem.id ?: return null,
+            convertTextToDate(responseItem.title) ?: return null,
+            responseItem.title ?: return null,
+            responseItem.author?.name ?: return null,
+            concatenateUrlWithBaseUrl(responseItem.relativeUrl) ?: return null,
+            responseItem.description ?: return null
+        )
+
+    }
+
+    private fun convertTextToDate(text: String?): Date? {
+        dateFormats.forEach { dateFormat ->
+            dateFormat.parseOrNull(text)?.let { return it }
+        }
+        return null
+    }
+
+    private fun concatenateUrlWithBaseUrl(url: String?): String? {
+        return url?.let {
+            "$baseUrl$url"
+        } ?: run {
+            null
+        }
+    }
+
+}
