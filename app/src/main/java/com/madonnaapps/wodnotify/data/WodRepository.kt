aeedf848a5b7ba7f23a448048010c99c92bfb311
Tyler Madonna <developer@madonnaapps.com>
2019-10-27 18:36:10 -0400

    Changed so remote data source is only synced after 15 minutes

--- app/src/main/java/com/madonnaapps/wodnotify/data/WodRepository.kt
+++ app/src/main/java/com/madonnaapps/wodnotify/data/WodRepository.kt
@@ -2,10 +2,12 @@ package com.madonnaapps.wodnotify.data
 
 import com.madonnaapps.wodnotify.data.local.WodLocalDataSource
 import com.madonnaapps.wodnotify.data.local.entities.WodEntity
+import com.madonnaapps.wodnotify.data.preferences.SharedPreferencesDataSource
 import com.madonnaapps.wodnotify.data.remote.WodRemoteDataSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
+import java.util.*
 
 interface WodRepository {
 
@@ -17,12 +19,21 @@ interface WodRepository {
 
 class WodRepositoryImpl constructor(
     private val wodLocalDataSource: WodLocalDataSource,
-    private val wodRemoteDataSource: WodRemoteDataSource
+    private val wodRemoteDataSource: WodRemoteDataSource,
+    private val sharedPreferencesDataSource: SharedPreferencesDataSource
 ) : WodRepository {
 
     override suspend fun getAllWods(): Flow<List<WodEntity>> = withContext(Dispatchers.IO) {
-        val remoteItems = wodRemoteDataSource.getWods()
-        wodLocalDataSource.insertWods(remoteItems)
+
+        val currentTime = Date().time
+
+        if (currentTime - sharedPreferencesDataSource.getLastWodSync() > 900000) {
+            // Only check if it's been 15 minutes since the last sync
+            val remoteItems = wodRemoteDataSource.getWods()
+            wodLocalDataSource.insertWods(remoteItems)
+            sharedPreferencesDataSource.saveLastWodSync(currentTime)
+        }
+
         return@withContext wodLocalDataSource.getAllWods()
     }
 
