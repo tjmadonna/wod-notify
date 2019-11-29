9cfd8457e6c70f87851f91318d3fd3f3e57e4dc6
Tyler Madonna <developer@madonnaapps.com>
2019-11-29 14:01:41 -0500

    Set up wod notification channel and wod periodic work in MainActivity

--- app/src/main/java/com/madonnaapps/wodnotify/ui/MainActivity.kt
+++ app/src/main/java/com/madonnaapps/wodnotify/ui/MainActivity.kt
@@ -2,16 +2,27 @@ package com.madonnaapps.wodnotify.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import com.madonnaapps.wodnotify.R
+import com.madonnaapps.wodnotify.common.extensions.appComponent
+import com.madonnaapps.wodnotify.notification.WodNotificationManager
+import com.madonnaapps.wodnotify.work.WodWorkManager
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var wodWorkManager: WodWorkManager
+
+    @Inject
+    lateinit var wodNotificationManager: WodNotificationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rv = RecyclerView(this)
+        wodNotificationManager.createWodSyncNotificationChannel()
+        wodWorkManager.createWodPeriodicWork()
     }
 
 }
