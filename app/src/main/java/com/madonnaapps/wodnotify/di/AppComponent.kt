fb244b8931e367a8c969722e6b81c26c63c0a58b
Tyler Madonna <developer@madonnaapps.com>
2019-11-29 14:00:45 -0500

    Added inject method for MainActivity in AppComponent

--- app/src/main/java/com/madonnaapps/wodnotify/di/AppComponent.kt
+++ app/src/main/java/com/madonnaapps/wodnotify/di/AppComponent.kt
@@ -2,6 +2,8 @@ package com.madonnaapps.wodnotify.di
 
 import android.content.Context
 import com.madonnaapps.wodnotify.di.modules.*
+import com.madonnaapps.wodnotify.ui.MainActivity
+import com.madonnaapps.wodnotify.work.worker.WodSyncWorker
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -14,7 +16,9 @@ import javax.inject.Singleton
         RepositoryModule::class,
         LocalDataModule::class,
         RemoteDataModule::class,
-        SharedPreferencesDataModule::class
+        SharedPreferencesDataModule::class,
+        NotificationModule::class,
+        WorkModule::class
     ]
 )
 interface AppComponent {
@@ -27,5 +31,10 @@ interface AppComponent {
 
     // Exposed subcomponent factories
     fun wodsFragmentComponentFactory(): WodsFragmentComponent.Factory
+
+    // Classes that can inject into component
+    fun inject(mainActivity: MainActivity)
+
+    fun inject(wodSyncWorker: WodSyncWorker)
 
 }
